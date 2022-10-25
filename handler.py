c24fd2b47bb5e5f1d0dc5972c59b5605aa0135b2
ErikBoesen <me@erikboesen.com>
2022-10-25 00:32:19 -0400

    Move bot code into handler.py

--- handler.py
+++ handler.py
@@ -1,24 +1,58 @@
+import sys
+sys.path.insert(0, 'vendor')
+
 import json
+import os
+import requests
+import mebots
+import random
+
+bot = mebots.Bot('mealbot', os.environ.get('BOT_TOKEN'))
+PREFIX = 'mealbot'
+GROUP_SIZE = 2
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def receive(event, context):
+    message = json.loads(event['body'])
+    group_id = message['group_id']
+    bot_id = message['bot_id']
+    user_token = message['user_token']
+    response = process(message, group_id, user_token)
+    if response:
+        send(response, bot_id)
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+        'statusCode': 200,
+        'body': 'ok',
     }
-    """
+
+
+def process(message, group_id, user_token):
+    # Prevent self-reply
+    if message['sender_type'] != 'bot':
+        if message['text'].lower().startswith(PREFIX):
+            group_id = message['group_id']
+            bot_id = instance.id
+            groupme_token = instance.token
+            users = requests.get(f'https://api.groupme.com/v3/groups/{group_id}?token={user_token}').json()['response']['members']
+            users = [user['name'] for user in users]
+            random.shuffle(users)
+            pairs = []
+            extra = None
+            if len(users) % GROUP_SIZE != 0:
+                extra = users.pop()
+            for i in range(0, len(users), GROUP_SIZE):
+                pairs.append(users[i:i + GROUP_SIZE])
+            if extra:
+                pairs[-1].append(extra)
+            return 'Meal pairings:\n--------------\n' + '\n'.join(['- ' + ' & '.join(pair) for pair in pairs])
+
+
+def send(text, bot_id):
+    url = 'https://api.groupme.com/v3/bots/post'
+
+    message = {
+        'bot_id': bot_id,
+        'text': text,
+    }
+    r = requests.post(url, json=message)
